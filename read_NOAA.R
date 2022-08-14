40d2b9598f783ca34e34ad516214692f74621c06
Siegfred Roi Lusuegro <76110917+slcodia@users.noreply.github.com>
2022-08-14 22:25:02 +0800

    Update read_NOAA.R
    Show error if file does not exist. Skips non-existent file to avoid interruption.

--- read_NOAA.R
+++ read_NOAA.R
@@ -1,5 +1,3 @@
-
-
 read_NOAA <- function(station, path, start = 1950, end = 2021, verbose = FALSE){
     
     if (verbose){begin.tiid <- Sys.time()}
@@ -35,74 +33,83 @@ read_NOAA <- function(station, path, start = 1950, end = 2021, verbose = FALSE){
     )
     
     for (year in start:end){
-        path0 <- file.path(path,year)
-        file <- paste0(station,"-99999-",year,".op.gz")
-        data <- read.fwf(gzfile(file.path(path0,file)),
-                         widths = c(6, #STN
-                                    6, #WBAN
-                                    6, #YEAR
-                                    2, #MONTH
-                                    2, #DAY
-                                    8, #TEMP
-                                    3, #Count
-                                    8, #DEWP
-                                    3, #Count
-                                    8, #SLP
-                                    3, #Count
-                                    8, #STP
-                                    3, #Count
-                                    7, #VISIB
-                                    3, #Count
-                                    7, #WDSP
-                                    3, #Count
-                                    7, #MXSPD
-                                    7, #GUST
-                                    8, #MAX
-                                    1, #Flag
-                                    7, #MIN,
-                                    1, #Flag
-                                    6, #PRCP
-                                    1, #Flag
-                                    6, #SNDP
-                                    8  #FRSHTT
-                         )
-                         , header = FALSE, skip = 1)
-        # renaming columns
-        names(data) <- c("STN", "WBAN","YEAR", "MONTH","DAY", 
-                         "TEMP", "TEMP.Count", 
-                         "DEWP", "DEWP.Count", 
-                         "SLP", "SLP.Count", 
-                         "STP", "STP.Count", 
-                         "VISIB", "VISIB.Count", 
-                         "WDSP", "WDSP.Count", 
-                         "MXSPD", "GUST", 
-                         "MAX", "MAX.Flag",
-                         "MIN", "MIN.Flag",
-                         "PRCP","PRCP.Flag",
-                         "SNDP", 
-                         "FRSHTT")
-        # creating date column
         
+        file <- paste0(station,"-99999-",year,".op.gz") # file name
+        file.path0 <- file.path(path,year,file) # complete file path
         
-        # getting the required columns only
-        data <- data[,c("STN","YEAR", "MONTH", "DAY", 
-                        "PRCP", "PRCP.Flag",
-                        "MIN", "TEMP", "MAX", 
-                        "DEWP", "SLP", "VISIB", "WDSP")]
-        
-        # creating Date column
-        # disabling warning temporarily
-        defaultW <- getOption("warn") 
-        options(warn = -1) 
-        data <- mutate(data, DATE = lubridate::make_date(YEAR,MONTH,DAY))
-        options(warn = defaultW)
-        
-        
-        data0 <- rbind(data0,data)
-        
-        if (verbose){
-            print(paste(year,"done!"))
+        if (file.exists(file.path0)){
+            data <- read.fwf(gzfile(file.path0),
+                             widths = c(6, #STN
+                                        6, #WBAN
+                                        6, #YEAR
+                                        2, #MONTH
+                                        2, #DAY
+                                        8, #TEMP
+                                        3, #Count
+                                        8, #DEWP
+                                        3, #Count
+                                        8, #SLP
+                                        3, #Count
+                                        8, #STP
+                                        3, #Count
+                                        7, #VISIB
+                                        3, #Count
+                                        7, #WDSP
+                                        3, #Count
+                                        7, #MXSPD
+                                        7, #GUST
+                                        8, #MAX
+                                        1, #Flag
+                                        7, #MIN,
+                                        1, #Flag
+                                        6, #PRCP
+                                        1, #Flag
+                                        6, #SNDP
+                                        8  #FRSHTT
+                             )
+                             , header = FALSE, skip = 1)
+            # renaming columns
+            names(data) <- c("STN", "WBAN","YEAR", "MONTH","DAY", 
+                             "TEMP", "TEMP.Count", 
+                             "DEWP", "DEWP.Count", 
+                             "SLP", "SLP.Count", 
+                             "STP", "STP.Count", 
+                             "VISIB", "VISIB.Count", 
+                             "WDSP", "WDSP.Count", 
+                             "MXSPD", "GUST", 
+                             "MAX", "MAX.Flag",
+                             "MIN", "MIN.Flag",
+                             "PRCP","PRCP.Flag",
+                             "SNDP", 
+                             "FRSHTT")
+
+            
+            
+            # getting the required columns only
+            data <- data[,c("STN","YEAR", "MONTH", "DAY", 
+                            "PRCP", "PRCP.Flag",
+                            "MIN", "TEMP", "MAX", 
+                            "DEWP", "SLP", "VISIB", "WDSP")]
+            
+            # creating Date column
+            # disabling warning temporarily
+            defaultW <- getOption("warn") 
+            options(warn = -1) 
+            data <- mutate(data, DATE = lubridate::make_date(YEAR,MONTH,DAY))
+            options(warn = defaultW)
+            
+            
+            data0 <- rbind(data0,data)
+            
+            if (verbose){
+                message(year," done!")
+            }   
+        }else{
+            if (verbose){
+                message(year," data does not exist.")
+            }   
         }
+
     }
     
     ## Date: Creating a sequence
@@ -116,19 +123,7 @@ read_NOAA <- function(station, path, start = 1950, end = 2021, verbose = FALSE){
                    by = 'DATE',
                    all = TRUE)
     
-    # counting missing dates
-    
-    if (verbose){
-        missing <- sum(is.na(data0$STN))
-        print(paste("A total of",missing,"days are missing from the data."))
-    }
-    
-    #filling station and date columns
-    
-    data0$STN   <- station
-    data0$YEAR  <- lubridate::year(data0$DATE)
-    data0$MONTH <- as.factor(lubridate::month(data0$DATE))
-    data0$DAY   <- lubridate::day(data0$DATE)
+
     
     
     
@@ -162,11 +157,53 @@ read_NOAA <- function(station, path, start = 1950, end = 2021, verbose = FALSE){
     ## Wind Speed
     data0$WDSP <- replace(data0$WDSP,data0$WDSP==999.9, NA)
     
-    print("DATA EXTRACTION SUCCESSFUL!")
+    # Filling date columns
+    data0$YEAR  <- lubridate::year(data0$DATE)
+    data0$MONTH <- as.factor(lubridate::month(data0$DATE))
+    data0$DAY   <- lubridate::day(data0$DATE)
+    
+    message("DATA EXTRACTION SUCCESSFUL!")
+    
     if (verbose) {
+        # running time
         time <- Sys.time() - begin.tiid
         print(time)
         
+        if (!require("beepr",character.only = TRUE)==FALSE){
+            # for the sound effect. Only run if beepr is installed
+            require(beepr)
+            beep()
+        }
+        
+        
+        # counting missing dates
+        missing <- sum(is.na(data0$STN))
+        message("A total of ",missing," days are missing from the data for the years ",start," to ",end,".")
+        
+        # count missing per variable
+        q <- menu(title = "Show number of missing values per year?",
+                  choices = c("Yes", "No"))
+        
+        if (q==1){
+            missing.data <- data0 %>% 
+                group_by(YEAR) %>%
+                summarize(DAYS = sum(is.na(STN)),
+                          PRCP = sum(is.na(PRCP)),
+                          MIN = sum(is.na(MIN)),
+                          TEMP = sum(is.na(TEMP)),
+                          MAX = sum(is.na(MAX)),
+                          DEWP = sum(is.na(DEWP)),
+                          SLP = sum(is.na(SLP)),
+                          VISIB = sum(is.na(VISIB)),
+                          WDSP = sum(is.na(WDSP)))
+            View(missing.data)
+        }
+
     }
+    
+    #filling station columns
+    data0$STN   <- station
+    
+    # final data
     return(data0)
 }
