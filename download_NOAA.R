d66fe8309ec83649c2fc68da23da2c4186a9fe83
Siegfred Roi Lusuegro <76110917+slcodia@users.noreply.github.com>
2022-08-28 21:52:17 +0800

    Update download_NOAA.R
    Added descriptions; changed `start` and `end` parameters to a single `year` parameter

--- download_NOAA.R
+++ download_NOAA.R
@@ -1,29 +1,62 @@
-getwd()
-download_NOAA <- function(station, dest.path =NULL, start = 1950, end = 2021, 
-                          timeout = 60, overwrite = F,verbose = FALSE){
-    # used to download data
-    # only to be run once
-    
-    # Station is USAF ID
-    # station numbers can be found here:  
-    #   ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.csv
-    # for example, Legazpi weather station's USAF ID is 984440
-    
-    # dest.path is where you want to store the files
-    # for example: 
-    ## dest.path <- "C:/Users/Siegfred Codia/Documents/Acads/MS Stat/Thesis/Data/NOAA/daily extracted"
 
+download_NOAA <- function(station, dest.path =NULL, years, 
+                          timeout = 60, overwrite = F,verbose = FALSE){
     
+    #' @description 
+    #' This functions automates downloading data from Global Surface Summary of the Day (GSOD) data
+    #' provided by National Oceanic and Atmospheric Administration (NOAA)
+    #' 
+    #' Data is extracted from their FTP server: `ftp://ftp.ncdc.noaa.gov/pub/data/gsod/`
+    #' 
+    #' Additional notes:
+    #' Only to be run once, unless you want to update your local data
+    #' So far, this function only extracts ONE STATION at a time
+    #' 
+    #' @author Siegfred Roi L. Codia, \email{slcodia@@up.edu.ph}
+        
+
+    #' @param station is USAF ID
+    #' station numbers can be found here: ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.csv
+    #' for example, Legazpi weather station's USAF ID is 984440
+    
+    #' @param dest.path is where you want to store the files
+    #' if `dest.path` is not specified, data will be downloaded to current working directory
+    #' for example: 
+    #' `dest.path <- "C:/Users/Siegfred Codia/Documents/Acads/MS Stat/Thesis/Data/NOAA/daily extracted"`
+    
+    #' @param years is a single number or vector of years
+    #' for example, `years = 1973:2021`, `years = c(1995,2005,2015)`
+    #' 
+    #' @param timeout is the time in seconds until download attempt is considered failed 
+    #' 
+    #' @param overwrite determines if we should overwrite current files, or we skip
+    #' tip: setting overwrite = T reduces download time and conserves bandwidth, 
+    #' if we are sure that current files are updated
+    #' 
+    #' @param verbose tracks download progress
+     
+    
+    #' @example 
+    #' # Download data from Iloilo Weather station for years 1973-2021
+    #' download_NOAA(station = 986460, dest.path = dest.path0, start = 1991, overwrite = F, timeout = 60,verbose = TRUE)
+    
+    # Clarifying download destination
     if (is.null(dest.path)){
         q <- menu(c(paste("Yes. Download data to",getwd()),
-                          "No. I will provide another destination path."),
-                    title = paste("dest.path is not specified. Extract to current directory?"))
+                    "No. I will provide another destination path."),
+                  title = paste("dest.path is not specified. Extract to current directory?"))
         if(q==1){
             dest.path <- getwd()
         }else{stop("Specify your destination path before proceeding.")}
     }
     
-    if (verbose){begin.tiid <- Sys.time()}
+    # Initializing Download time
+    if (verbose){
+        #running time
+        begin.tiid <- Sys.time()
+        # file download counter
+        count <- 0
+    }
     
     require(RCurl) # for locating a url path, for url.exists
     require(curl)
@@ -37,116 +70,126 @@ download_NOAA <- function(station, dest.path =NULL, start = 1950, end = 2021,
     
     
     # creating a safe function for curl_fetch_memory
-    
     s_curl_fetch_memory <- safely(curl_fetch_memory)
     
-    for (year in start:end){
-        # success indicator: s = 0 fail, s = 1 success
-        s = 0
+    # iterating through the years
+    for (year in years){
         
-        # fetch the year
-        year_url <- sprintf(ftp_base, year)
+        # file name to extract
+        filename <- paste0(station,"-99999-",year,".op.gz")
         
-        # FETCHING FILE NAMES
-        time.start <- Sys.time()
-        repeat {
-            
-            
-            # curl_fetch_memory write data from URL into a memory (using the safe cfm function)
-            res <- s_curl_fetch_memory(year_url, handle=dir_list_handle)
-            
-            # if result of fetching fails, process will repeat
-            if (!is.null(res$result)){
-                tmp <- res$result
-                con <- rawConnection(tmp$content) # create connection
-                files <- readLines(con) # list of the files in the folder
-                close(con) # close connection to reduce memory
-                s = 1
-                
-                break # break repeat if connection has been found
-            }
-            
-            # until when will the repeat loop run?
-            if (difftime(Sys.time(), time.start, units = "secs")>timeout){ 
-                
-                message("Fetching memory failed at year ", year," due to timeout.")
-                break
-            }
-        }
+        # Server files
+            # fetch the year url
+            year_url <- sprintf(ftp_base, year)
+            # file to download
+            file_url <- file.path(year_url,filename)
         
-        # if connection to ftp has been found
-        if (s == 1){
-            if (verbose){
-                message(paste0("Successfuly connected to year folder ",year, ". Will now find file..."))
-            }
-            
-            
+        # Local files
             # location of destination
             year_loc <- file.path(dest.path0,year)
+            # put file on cache
+            cache_loc <- file.path(year_loc,filename)
+        
+        
+        #checking existence of local files helps us conserve bandwidth, instead of checking server then download...
+        
+        if(file.exists(cache_loc)&(overwrite==F)){
+        # skip download if cache_file already exists (depending on the value of `overwrite`)
+            if(verbose){
+                message(paste(filename,"already exists in local folder. Skipping download..."))
+            }
+            next
             
-            # file name to extract
-            filename <- paste0(station,"-99999-",year,".op.gz")
             
-            # file to download
-            loc <- file.path(year_url,filename)
+        }else if(file.exists(cache_loc)&(overwrite==T)){
+        # if it exists in local folder, it is assumed that it also exists in the server.
+            message(paste(filename,"already exists in local folder. Overwriting..."))
             
+            d <- tryCatch(download.file(file_url,cache_loc), 
+                          error = function(e) print(paste("Download failed for year",year)))
+            if(d == 0){
+                if(verbose){
+                    print(paste(year, "successfuly overwritten!"),quote = F)
+                    count = count+1
+                }
+            }
+        
+             
+        }else if(!file.exists(cache_loc)){
+        # if file DOES NOT exist in our local folder YET, we need to then confirm if it exists in the server  
+            
+            # creating a safe curl fetch memory function
+            s_curl_fetch_memory <- safely(curl_fetch_memory)
+            
+            # FETCHING FILE NAMES
+            time.start <- Sys.time()
+            repeat {
                 
+                # curl_fetch_memory write data from URL into a memory (using the safe cfm function)
+                res <- s_curl_fetch_memory(year_url, handle=dir_list_handle)
+                
+                # if result of fetching is successful,  repeat will end
+                
+                if (!is.null(res$result)){
+                    tmp <- res$result
+                    con <- rawConnection(tmp$content) # create connection
+                    files <- readLines(con) # list of the files in the folder
+                    if (verbose){message(paste0("Successfuly connected to year folder ",year))}
+                    close(con) # close connection to reduce memory
+                    break # break repeat if connection has been found
+                }
+                
+                # until when will the repeat loop run?
+                if (difftime(Sys.time(), time.start, units = "secs")>timeout){ 
+                    
+                    if (verbose) {
+                        # error sound
+                        system("rundll32 user32.dll,MessageBeep -1")
+                        runtime <- difftime(Sys.time(),begin.tiid, units = "mins")
+                        print(paste("Succesfully downloaded", count, "files"), quote = F)
+                        print(paste("Running time:",round(runtime,2),"minutes."), quote = F)
+                        
+                    }
+                    stop(paste("Fetching memory failed at year ", year," due to timeout."), call. = F)
+                    
+                }
+            }# end repeat
             
-            
-            # locating if file exists
             if (filename %in% files){
                 if(verbose){
                     message(paste("File",filename,"found. Proceeding to download..."))
                 }
-                # Proceeding to download...
                 
-                # put file on cache
-                cache_file <- file.path(year_loc,filename)
+                d <- tryCatch(download.file(file_url,cache_loc), 
+                              error = function(e) print(paste("Download failed for year",year)))
                 
-                #skip download if cache_file already exists (depending on the value of `overwrite`)
-                if(file.exists(cache_file)&(overwrite==T)){
-                    message(paste(filename,"already exists in local folder. Overwriting..."))
-                    
-                    d <- tryCatch(download.file(loc,cache_file), 
-                                  error = function(e) print(paste("Download failed for year",year)))
-                    if(d == 0){
-                        if(verbose){print(paste(year, "successfuly overwritten!"),quote = F)}
+                if(d == 0){
+                    if(verbose){
+                        print(paste(year, "successfuly downloaded!"), quote = F)
+                        count = count+1
                     }
-                    
-                }else if(file.exists(cache_file)&(overwrite==F)){
-                    if(verbose){message(paste(filename,"already exists in local folder. Skipping download..."))}
-                    next
-                    
-                }else if(!file.exists(cache_file)){
-                    d <- tryCatch(download.file(loc,cache_file), 
-                                  error = function(e) print(paste("Download failed for year",year)))
-                    if(d == 0){
-                        if(verbose){print(paste(year, "successfuly downloaded!"), quote = F)}
-                    }
-                }
-                
+                }else{next}# proceed to next year if download fails.
                 
             }else{
                 print(paste("Data for year", year,"does not exist."), quote = F)
+                
             } 
-        }else{
             
-            stop("Connection to ftp server lost. Server may be under loaded.")
         }
+        
+            
         
         
     }
     
     if (verbose) {
-        runtime <- difftime(Sys.time(),begin.tiid, units = "secs")
-        if (runtime < 120){
-            print(paste("Running time:",round(runtime,2),"seconds."), quote = F)
-        }else{
-            print(paste("Running time:",round(runtime/60,2),"minutes."), quote = F)
-        }
-    }
+        beepr::beep(sound = 1, expr = NULL)
+        runtime <- difftime(Sys.time(),begin.tiid, units = "mins")
+        print(paste("Succesfully downloaded", count, "files"), quote = F)
+        print(paste("Running time:",round(runtime,2),"minutes."), quote = F)
         
+    }
+    
 }
-
 
 
